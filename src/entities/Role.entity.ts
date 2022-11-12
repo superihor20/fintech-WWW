e3e495609f28c30f57917acaa418536079cc6e18
Ihor Naidonov <superihor20@gmail.com>
2022-11-12 21:55:52 +0200

    relation beetwen role and permissions migration

--- src/entities/Role.entity.ts
+++ src/entities/Role.entity.ts
@@ -1,6 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 import { UserRoles } from '../common/enums/user-roles.enum';
+
+import { Permission } from './Permission.entity';
 
 @Entity('roles')
 export class Role {
@@ -9,4 +17,12 @@ export class Role {
 
   @Column({ type: 'enum', enum: UserRoles, unique: true })
   name: UserRoles;
+
+  @ManyToMany(() => Permission, { eager: true })
+  @JoinTable({
+    name: 'role_to_permission',
+    joinColumns: [{ name: 'role_id', referencedColumnName: 'id' }],
+    inverseJoinColumns: [{ name: 'permission_id', referencedColumnName: 'id' }],
+  })
+  permissions: Permission[];
 }
