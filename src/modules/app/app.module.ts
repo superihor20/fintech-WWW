1aef765fc7629eb663ff68c4a426ddea86c2de53
Ihor Naidonov <superihor20@gmail.com>
2022-11-12 09:52:03 +0200

    connect db to app

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        logging: true,
+        entities: [],
+        migrations: [],
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
