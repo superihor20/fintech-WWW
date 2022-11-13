4302116213f4590997014ca801c5e719bff28e5c
Ihor Naidonov <superihor20@gmail.com>
2022-11-13 16:09:35 +0200

    update db design

--- src/entities/Role.entity.ts
+++ src/entities/Role.entity.ts
@@ -1,15 +1,7 @@
-import {
-  Column,
-  Entity,
-  JoinTable,
-  ManyToMany,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 import { UserRoles } from '../common/enums/user-roles.enum';
 
-import { Permission } from './Permission.entity';
 import { User } from './User.entity';
 
 @Entity('roles')
@@ -22,12 +14,4 @@ export class Role {
 
   @OneToMany(() => User, (user) => user.id)
   users: User[];
-
-  @ManyToMany(() => Permission, { eager: true })
-  @JoinTable({
-    name: 'role_to_permission',
-    joinColumns: [{ name: 'role_id', referencedColumnName: 'id' }],
-    inverseJoinColumns: [{ name: 'permission_id', referencedColumnName: 'id' }],
-  })
-  permissions: Permission[];
 }
