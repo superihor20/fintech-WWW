68ef25e611171525624446fd68e2b1799c582774
Ihor Naidonov <superihor20@gmail.com>
2022-11-13 16:15:35 +0200

    update migrations

---
+++ src/migrations/1668348843465-RoleTable.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class RoleTable1668348843465 implements MigrationInterface {
+  name = 'RoleTable1668348843465';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TYPE "public"."roles_name_enum" AS ENUM('user', 'investor', 'inviter', 'admin')
+        `);
+    await queryRunner.query(`
+            CREATE TABLE "roles" (
+                "id" SERIAL NOT NULL,
+                "name" "public"."roles_name_enum" NOT NULL,
+                CONSTRAINT "UQ_648e3f5447f725579d7d4ffdfb7" UNIQUE ("name"),
+                CONSTRAINT "PK_c1433d71a4838793a49dcad46ab" PRIMARY KEY ("id")
+            )
+        `);
+    await queryRunner.query(`
+            ALTER TABLE "users"
+            ADD "role_id" integer
+        `);
+    await queryRunner.query(`
+            ALTER TABLE "users"
+            ADD CONSTRAINT "FK_a2cecd1a3531c0b041e29ba46e1" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "users" DROP CONSTRAINT "FK_a2cecd1a3531c0b041e29ba46e1"
+        `);
+    await queryRunner.query(`
+            ALTER TABLE "users" DROP COLUMN "role_id"
+        `);
+    await queryRunner.query(`
+            DROP TABLE "roles"
+        `);
+    await queryRunner.query(`
+            DROP TYPE "public"."roles_name_enum"
+        `);
+  }
+}
