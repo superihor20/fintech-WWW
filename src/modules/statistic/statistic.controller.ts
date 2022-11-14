40cd3f839033e63b63e7afdc65b639b7c6d4d7ce
Ihor Naidonov <superihor20@gmail.com>
2022-11-14 13:56:05 +0200

    added guard for statistic

--- src/modules/statistic/statistic.controller.ts
+++ src/modules/statistic/statistic.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+
+import { UserRoles } from '../../common/enums/user-roles.enum';
+import { Roles } from '../../decorators/role.decorator';
+import { RolesGuard } from '../../guards/roles.guards';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 import { StatisticService } from './statistic.service';
 
+@Roles(UserRoles.ADMIN)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('statistic')
 export class StatisticController {
   constructor(private readonly statisticService: StatisticService) {}
