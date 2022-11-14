271fba14cf5eb4a8a5f5dbdf2feab3e6effe3f2d
Ihor Naidonov <superihor20@gmail.com>
2022-11-14 16:16:19 +0200

    added guards for statistic

--- src/modules/statistic/statistic.module.ts
+++ src/modules/statistic/statistic.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { UserModule } from '../user/user.module';
+
 import { StatisticController } from './statistic.controller';
 import { StatisticService } from './statistic.service';
 
 @Module({
+  imports: [UserModule],
   controllers: [StatisticController],
   providers: [StatisticService],
 })
--- src/modules/statistic/statistic.service.ts
+++ src/modules/statistic/statistic.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+import { UserRoles } from '../../common/enums/user-roles.enum';
+import { User } from '../../entities';
+import { UserService } from '../user/user.service';
+
 @Injectable()
 export class StatisticService {
-  getStatistic() {
-    return 'statistic';
+  constructor(private readonly userService: UserService) {}
+
+  async getStatistic(): Promise<{
+    numberOfAllUsers: number;
+    numberOfInvestors: number;
+    investors: User[];
+  }> {
+    const [, numberOfAllUsers] = await this.userService.findWithFilter();
+    const [investors, numberOfInvestors] =
+      await this.userService.findWithFilter({
+        where: { role: { name: UserRoles.INVESTOR } },
+      });
+
+    return {
+      numberOfAllUsers,
+      numberOfInvestors,
+      investors,
+    };
   }
 }
