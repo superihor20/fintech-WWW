e537498e862fa9967832161c4ebeecf877badd3e
Ihor Naidonov <superihor20@gmail.com>
2022-11-16 18:23:13 +0200

    syntax fix

--- src/common/enums/operation-type.enum.ts
+++ src/common/enums/operation-type.enum.ts
@@ -2,5 +2,5 @@ export enum OperationType {
   DEPOSITE = 'deposite',
   WITHDRAW = 'withdraw',
   DAILY_INCREASE = 'daily increase',
-  INVITER_BONUS = 'invter bonus',
+  INVITER_BONUS = 'inviter bonus',
 }
